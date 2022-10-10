b031a5a8e5c572eb5abdf2a22e7d6d55c57b4b27
Marina <114186546+MarinaM7@users.noreply.github.com>
2022-10-10 17:04:03 +0200

    Add files via upload

---
+++ start1/js/index.js
@@ -0,0 +1,9 @@
+/*
+
+alert('Usa il pop up per dare dei messaggi');
+
+window.alert('Sto studiando JS');
+
+alert('Chiudimi')
+
+*/
---
+++ start1/js/naming.js
@@ -0,0 +1,45 @@
+var pet_preferito = 'gattino';
+console.log(pet_preferito);
+
+var pet = 'gatto';
+var Pet = 'cane';
+
+console.log(pet);
+console.log(Pet);
+
+
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+console.log(petPreferito);
+console.log(PetPreferito);
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+
+
+var amazing = 'You changed the title'
+
+function cliccami() {
+    document.getElementById('change').innerHTML = "Alright, this is awesom";
+    document.getElementById('text').innerHTML = (amazing);
+    alert('Close to continue !!');
+    console.log(amazing);
+}
+
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+var numero1 = '13';
+document.getElementById('first').innerHTML = (numero1)
+
+var numero2 = '14'
+document.getElementById('second').innerHTML = (numero2)
+
+var longtext = 'The Palace of Versailles. A decadent symbol of royal power and influence that, to this day, still wows visitors. It’s one of the most-visited attractions in Paris. The Palace offers an incredible insight into French history, illuminating the lavish and ostentatious lives the former monarchs used to live.'
+
+function caption() {
+    document.getElementById('vers').innerHTML = (longtext);
+}
